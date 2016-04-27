6251c357b9f4796eb00796afbb460b2206bf1b72
Vladimir Panteleev <git@thecybershadow.net>
2016-04-27 00:50:27 +0000

    Initial commit

---
+++ client.d
@@ -0,0 +1,140 @@
+import core.thread;
+
+import std.exception;
+import std.getopt;
+import std.path;
+import std.process;
+import std.stdio;
+import std.string;
+
+import ae.sys.d.manager;
+import ae.sys.file;
+import ae.utils.array;
+import ae.utils.json;
+
+import common.common;
+
+class TestDManager : DManager
+{
+	this()
+	{
+		config.local.workDir = "work".absolutePath();
+		config.local.workDir.ensurePathExists();
+		config.local.cache = "git";
+	}
+
+	override void log(string line)
+	{
+		sendLog(Message.Log.Type.log, line);
+	}
+
+	override string getCallbackCommand() { assert(false); } // Not needed in this program
+}
+
+void sendLog(Message.Log.Type type, string text)
+{
+	Message message = { type : Message.Type.log, log : { type : type, text : text } };
+	sendMessage(message);
+}
+
+void sendProgress(Message.Progress.Type type)
+{
+	Message message = { type : Message.Type.progress, progress : { type : type } };
+	sendMessage(message);
+}
+
+File realStdout; static this() { realStdout.fdopen(1, "wb"); }
+
+void sendMessage(ref Message message)
+{
+	realStdout.writeln(message.toJson());
+	realStdout.flush();
+}
+
+void redirect(ref File file, Message.Log.Type type)
+{
+	auto pipes = [pipe()].ptr;
+	file = pipes.writeEnd;
+	auto t = new Thread({
+		string line;
+		while ((line = pipes.readEnd.readln()) !is null)
+			sendLog(type, line.chomp());
+	});
+	t.isDaemon = true;
+	t.start();
+}
+
+int main(string[] args)
+{
+	try
+	{
+		redirect(stdout, Message.Log.Type.stdout);
+		redirect(stderr, Message.Log.Type.stderr);
+
+		string clientId;
+		string[] fetchList, mergeList;
+
+		getopt(args,
+			"id", &clientId,
+			"fetch", &fetchList,
+			"merge", &mergeList,
+		);
+
+		enforce(args.length == 2, "Expected one non-option argument (base commit)");
+		auto base = args[1];
+
+		enforce(clientId, "Client ID not specified!");
+
+		auto d = new TestDManager;
+
+		sendProgress(Message.Progress.Type.fetch);
+
+		// To avoid race conditions, we use the SHA1s from the command line.
+		// We only pull these (and throw away the result) so that we have said SHAs
+		// (in lieu of git/github providing a way to fetch a SHA1 directly).
+		foreach (fetchItem; fetchList)
+		{
+			string submoduleName, remote, refString;
+			list(submoduleName, remote, refString) = fetchItem.split('|');
+			d.getSubmodule(submoduleName).getRemoteRef(remote, refString, "FETCH_HEAD");
+		}
+
+		sendProgress(Message.Progress.Type.merge);
+
+		auto submoduleState = d.begin(base);
+
+		foreach (mergeItem; mergeList)
+		{
+			string submoduleName, branch;
+			list(submoduleName, branch) = mergeItem.split('|');
+			d.merge(submoduleState, submoduleName, branch);
+		}
+
+		sendProgress(Message.Progress.Type.build);
+
+		foreach (componentName; d.allComponents)
+			d.config.build.components.enable[componentName] = shouldBuild(clientId, componentName);
+
+		d.build(submoduleState);
+
+		sendProgress(Message.Progress.Type.test);
+
+		foreach (componentName; d.allComponents)
+			d.config.build.components.enable[componentName] = shouldTest(clientId, componentName);
+
+		d.test();
+
+		sendProgress(Message.Progress.Type.bench);
+
+		// TODO: benchmarks
+		
+		sendProgress(Message.Progress.Type.done);
+
+		return 0;
+	}
+	catch (Exception e)
+	{
+		sendLog(Message.Log.Type.error, e.toString());
+		return 1;
+	}
+}
